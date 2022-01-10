9a1da00f3b5e7bc758557ccfc31bd201ec28a2cb
galeno <gui.galenocover@gmail.com>
2022-01-10 15:21:53 -0300

    JSX in a JS Function

--- src/components/basics/First.js
+++ src/components/basics/First.js
@@ -1,3 +1,14 @@
-export default function First(){ //This function could be anonymous, but would still work
-    return 'First Component'
+import React from "react";
+
+export default function First() {
+  //This function could be anonymous, but would still work
+  const msg = 'Welcome'
+  return ( //to keep the code indented
+    <div>
+      <h2>First Component</h2> {/*JSX with a function JS*/}
+      <p>
+          {msg}
+      </p>
+    </div>
+  );
 }
