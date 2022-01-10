243c562defde1a51d0c3ac0380310968bcc49fc7
galeno <gui.galenocover@gmail.com>
2022-01-10 15:13:05 -0300

    Adding JS in JXS syntax

---
+++ src/components/basics/First.js
@@ -0,0 +1,3 @@
+export default function First(){ //This function could be anonymous, but would still work
+    return 'First Component'
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import './index.css' //Importing CSS
 import ReactDOM from "react-dom"; //Importing ReactDom | Document Object Model
 import React from "react"; //Syntax JSX
+import First from './components/basics/First'; //Exporting JS elements
+//If First was called "Dagoberto", would still work (only if the <First> tag is renamed)
 
+// #Examples Fundamentals of React ⤵
+/*
 const el = document.getElementById("root"); //Getting the element by id 'root'
-ReactDOM.render(<div> Olá </div>, el); // and rendering it in that element
+ReactDOM.render(<div> Hello </div>, el); // and rendering it in that element
 
 //But, you can make it directly like this
 ReactDOM.render(
   <div>
-    <b>React! {/* Using JSX to render HTML tag's in JS */}</b>
+<b>React! {/* Using JSX to render HTML tag's in JS */ /*}</b>
   </div>,
   document.getElementById("root2")
 );
@@ -16,3 +20,8 @@ ReactDOM.render(
 //You can make this using a variable like this:
 const tag = <h1>Let's go</h1>;
 ReactDOM.render(<div>{tag}</div>, document.getElementById("root3"));
+*/
+
+ReactDOM.render(<div>
+    <First></First> 
+    </div>, document.getElementById("root"));
