5c4b44e2ce0ec0a0bee31dae823c00a262f24c06
galeno <gui.galenocover@gmail.com>
2022-01-10 16:22:34 -0300

    Using Fragment in React Project

---
+++ src/components/basics/Fragment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Fragment(props) {
+  return (
+    // Or you can use "<></>""
+    <React.Fragment>
+      <h2>Fragment</h2>
+      <p>Beware of this error</p>
+    </React.Fragment>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import "./index.css"; //Importing CSS
 import ReactDOM from "react-dom"; //Importing ReactDom | Document Object Model
 import React from "react"; //Syntax JSX
-import First from "./components/basics/First"; //Exporting JS elements
+
+import First from "./components/basics/First"; //Exporting JS elements, if First was called "Dagoberto", would still work (only if the <First> tag is renamed)
 import WithParams from "./components/basics/WithParams";
-//If First was called "Dagoberto", would still work (only if the <First> tag is renamed)
-
-// #Examples Fundamentals of React ⤵
-/*
-const el = document.getElementById("root"); //Getting the element by id 'root'
-ReactDOM.render(<div> Hello </div>, el); // and rendering it in that element
-
-//But, you can make it directly like this
-ReactDOM.render(
-  <div>
-<b>React! {/* Using JSX to render HTML tag's in JS */ /*}</b>
-  </div>,
-  document.getElementById("root2")
-);
-
-//You can make this using a variable like this:
-const tag = <h1>Let's go</h1>;
-ReactDOM.render(<div>{tag}</div>, document.getElementById("root3"));
-*/
+import Fragment from "./components/basics/Fragment";
 
 ReactDOM.render(
-  // Here ⤵ I'm generating two props with their respective values, and in WithParams.jsx calling it for interpolation
-  <div>
+  // ⤵  Or you can use "<React.Fragment><React.Fragment/>""
+  <>
+    {/*Here ⤵ I'm generating two props with their respective values, and in WithParams.jsx calling it for interpolation*/}
     <First></First>
-    <WithParams title="Student's Situation" studentName="Peter" studentScore={9.3} />
- 
-    <WithParams title="Student's Situation" studentName="Michele" studentScore={9.7} />
+    <WithParams
+      title="Student's Situation"
+      studentName="Peter"
+      studentScore={9.3}
+    />
 
-    <WithParams title="Student's Situation" studentName="Flash" studentScore={6.1} />
-  </div>,
+    <WithParams
+      title="Student's Situation"
+      studentName="Michele"
+      studentScore={9.7}
+    />
+
+    <WithParams
+      title="Student's Situation"
+      studentName="Flash"
+      studentScore={6.1}
+    />
+  </>,
   document.getElementById("root")
 );
