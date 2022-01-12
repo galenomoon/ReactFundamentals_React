83777c4102a17d8eef29a014c40eafceed4b7fc4
galeno <gui.galenocover@gmail.com>
2022-01-12 13:17:28 -0300

    Indirect Communication via useState

--- src/components/communication/IndirectChild.jsx
+++ src/components/communication/IndirectChild.jsx
@@ -3,25 +3,30 @@ import React from "react";
 //Making the props to send to parent via props
 export default (props) => {
   const cb = props.whenClick;
+  let nerd = false;
+  const generateAge = () => parseInt(Math.random() * 20) + 50;
+  let generateNerd = () => Math.random() > 0.5;
   return (
     <div>
       <h3>Child</h3>
       {/*Getting the ⤵ function via props and showing it in the console*/}
-      <button onClick={(_) => cb("Jhonny", 53, true)}>Give Information</button>
+      <button onClick={(_) => cb("Jinny", generateAge, generateNerd)}>
+        Give Information
+      </button>
     </div>
   );
 };
 
 /*
-FATHER => FunctionToSon() => SON 
-                                ⤵
-                                Get de function
-                                and for a event 
-  Father get                   send a information
-  information                  to father
-          ↸                      ↲
-        Parent information
-        is queried and loaded 
-        by the child function
-
+================== HOW WORKS INDIRECT COMMUNICATION ===================
+              FATHER => FunctionToSon() => SON 
+                                              ⤵
+                                              Get de function
+                                              and for a event 
+                Father get                   send a information
+                information                  to father
+                        ↸                      ↲
+                      Parent information
+                      is queried and loaded 
+                      by the child function
 */
--- src/components/communication/IndirectParent.jsx
+++ src/components/communication/IndirectParent.jsx
@@ -1,35 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import IndirectChild from "./IndirectChild";
 
-// It's a Indirect ⤵ communication, cause it's done parent-to-child
-function giveInformation(name, age, nerd) {
-  console.log(name, age, nerd);
-}
+// It's a Indirect
+//⤵ communication, cause it's done parent-to-child
 export default (props) => {
-    let name = '?';
-    let age = 0;
-    let nerd = false;
+  // useState receive two values
+  // like this: ⤵ | [value, function]
+  const [a, b] = [1, 2];
+  const [name, setName] = useState("?");
+  const [age, setAge] = useState(0);
+  const [nerd, setNerd] = useState(false);
+
+  function giveInformation(name, age, nerd) {
+    // ⤵ Here we're calling the value via function
+    setName(name); // [name, setName]
+    setAge(age); // [age, setAge]
+    setNerd(nerd); // [nerd, setNerd]
+  }
+
   return (
     <div>
       <span>{name} </span>
       <span>{age}</span>
-      <span> {nerd ? 'True' : 'False'}</span>
+      <span> {nerd ? "True" : "False"}</span>
       <IndirectChild whenClick={giveInformation}></IndirectChild>
     </div>
   );
 };
 
 /*
-FATHER => FunctionToSon() => SON 
-                                ⤵
-                                Get de function
-                                and for a event 
-  Father get                   send a information
-  information                  to father
-        ↸                        ↲
-        Parent information
-        is queried and loaded 
-        by the child function
-
+================== HOW WORKS INDIRECT COMMUNICATION ===================
+           
+                FATHER => FunctionToSon() => SON 
+                                              ⤵
+                                              Get de function
+                                              and for a event 
+                Father get                   send a information
+                information                      to father
+                        ↸                           ↲
+                            Parent information
+                            is queried and loaded 
+                            by the child function
 */
-
